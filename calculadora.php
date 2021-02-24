f96bb82b3f96dada8f8f5c02904ab5a41fe10c88
Emmanuel Mercado <34379958+truedamagee@users.noreply.github.com>
2021-02-24 18:56:15 -0300

    Create calculator in php
    together whit calculadora.php

---
+++ calculadora.php
@@ -0,0 +1,16 @@
+<?php
+	$operando1 = $_GET['operando1'];
+	$operando2 = $_GET['operando2'];
+	$operador = $_GET['operador'];
+	
+	if($operador == "+"){
+		$solucion = $operando1 + $operando2;
+	}else if($operador == "-"){
+		$solucion = $operando1 - $operando2;
+	}else if($operador == "*"){
+		$solucion = $operando1 * $operando2;
+	}else if($operador == "/"){
+		$solucion = $operando1 / $operando2;
+	}
+	echo "La solución es: ".$solucion;
+?>
