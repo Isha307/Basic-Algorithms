183eaab7f0b3fc28f9f6014f5f613c5ede8c7d7a
knekvasil <kajnekvasil@gmail.com>
2021-03-06 13:00:44 -0500

    fixed sorting bug

--- MergeSort.py
+++ MergeSort.py
@@ -2,7 +2,7 @@
 
 def user_input():
 	user_data = input("Enter the elements in the array to be sorted (separated by spaces): ")
-	arr = user_data.split()
+	arr = [int(x) for x in user_data.split()]
 	print(f"The input array was: {arr}\nThe sorted array is: {merge_sort(arr)}")
 	
 
@@ -11,10 +11,11 @@ def merge_sort(arr):
 		midpoint = len(arr)//2
 		left_arr = arr[:midpoint]
 		right_arr = arr[midpoint:]
-		i,j,k = 0,0,0
 
 		merge_sort(left_arr)
 		merge_sort(right_arr)
+
+		i,j,k = 0,0,0
 
 		while i < len(left_arr) and j < len(right_arr):
 			if left_arr[i] < right_arr[j]:
@@ -34,9 +35,10 @@ def merge_sort(arr):
 			arr[k] = right_arr[j]
 			j += 1
 			k += 1
-
 	return arr
+
 
 if __name__ == "__main__":
 	user_input()
+
 	
